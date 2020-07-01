3a6bac5a4c0d18e61b116a3b54169d14bd92f90e
Sam Tovey <samtovey13@gmail.com>
2020-07-01 10:58:04 +0100

    solved arrays/ ALL

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,88 @@
 const getNthElement = (index, array) => {
-  // your code here
+  while (index >= array.length) {
+    index -= array.length;
+  }
+  return array[index];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array = array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  const newArray = [...array];
+  newArray.push(element);
+  return newArray;
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  function upper(x) {
+    return x.toUpperCase();
+  }
+  return strings.map(upper);
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  function reverseString(x) {
+    return x.split("").reverse().join("");
+  }
+  return strings.map(reverseString);
 };
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(num => num%2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const spliceArr = [...array]; // creates a clone //
+  spliceArr.splice(index, 1);
+  return spliceArr;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  function startsWithVowel(str) {
+    var vowelRegex = '^[aieouAIEOU].*'
+    if (str.match(vowelRegex)) {
+      return str;
+    }
+  }
+  return strings.filter(startsWithVowel)
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.replace(/\s/g, '');
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  const sum = numbers.reduce(function(total, num) { // arguments are total and currentValue//
+    return total + num;
+  }, 0);
+  return sum;
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  function lastCharVal(str) {
+    return str.charCodeAt(str.length - 1);
+  }
+  return strings.sort(function(a, b){
+    return lastCharVal(a) - lastCharVal(b);
+  });
 };
 
 module.exports = {
